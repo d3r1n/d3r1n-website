3f21d763eda33c3887e8f70032a71e3aead2246a
Derin Önder Eren <derinondereren@gmail.com>
2022-11-16 18:27:02 +0300

    Update: I hate tsc

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,15 @@
 </template>
 
 <script lang="ts">
-
 </script>
 
 <style lang="scss">
 body, html, #app {
   height: 100%;
 }
-
 body {
 	margin: 0;
 	padding: 0;
 	font-family: 'JetBrains Mono', monospace;
 }
 </style>
- 
