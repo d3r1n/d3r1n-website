5be280cc7809138131063a21ca93173b7d02ac6f
Derin Önder Eren <derinondereren@gmail.com>
2022-12-04 18:56:01 +0300

    Update: Save theme state to local storage

--- src/store/theme.ts
+++ src/store/theme.ts
@@ -1,8 +1,9 @@
 import { defineStore } from "pinia";
+import { useStorage } from "@vueuse/core";
 import { ref, Ref } from "vue"
 
 export const useTheme = defineStore("theme", () => {
-	const theme: Ref<boolean> = ref(true)
+	const theme: Ref<boolean> = useStorage("theme", false);
 
 	function toggle_theme() {
 		theme.value = !theme.value
