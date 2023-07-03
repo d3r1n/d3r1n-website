8813de286631d122591cc2e42a18823512c55e85
d3r1n <derinondereren@gmail.com>
2023-07-03 21:29:35 +0300

    feat: meta tags

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
         <Transition
             :enter-active-class="route.meta.enter as string"
             :leave-active-class="route.meta.leave as string"
-            v-on:after-enter="show = true"
+            :after-enter="() => (show_ = true)"
         >
             <KeepAlive>
                 <component :is="Component" :key="route.path" />
@@ -18,21 +18,10 @@
     const themeStore = useThemeStore()
     const theme = computed(() => themeStore.theme)
 
-    const show = ref(false)
+    const show_ = ref(false)
 
     useHead({
         titleTemplate: (title?: string) => (title ? `${title} | d3r1n.com` : 'd3r1n.com'),
-        meta: [
-            { name: 'description', content: "d3r1n's portfolio, built with Vue.js, TypeScript and UnoCSS" },
-            { name: 'keywords', content: 'd3r1n, portfolio, derin onder eren' },
-            { name: 'og:title', content: 'd3r1n.com' },
-            { name: 'og:description', content: "d3r1n's portfolio, built with Vue.js, TypeScript and UnoCSS" },
-            { name: 'og:type', content: 'website' },
-            { name: 'og:url', content: 'https://d3r1n.com' },
-            { name: 'og:image', content: `https://d3r1n.com/banner.png?v=${Date.now()}` },
-            { name: 'og:site_name', content: 'd3r1n.com' },
-            { name: 'twitter:card', content: 'summary_large_image' }
-        ],
         link: [{ rel: 'icon', type: 'image/svg+xml', sizes: 'any', href: '/favicon.svg' }]
     })
 
@@ -48,7 +37,7 @@
     })
 
     onMounted(() => {
-        show.value = true
+        show_.value = true
     })
 </script>
 
